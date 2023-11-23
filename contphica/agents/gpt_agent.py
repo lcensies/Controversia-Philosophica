065395cd45a42637b8c8dd6151e5ee9c45933c21
Lumiwarum <78841564+Lumiwarum@users.noreply.github.com>
2023-11-24 01:17:45 +0300

    Add files via upload

--- contphica/agents/gpt_agent.py
+++ contphica/agents/gpt_agent.py
@@ -1,4 +1,5 @@
 from contphica.agents.debate_agent import DebateAgent
+from contphica.agents.hf_initiator import hugging_face_initiator
 from os import environ
 
 # from langchain.schema import BaseLanguageModel
@@ -7,23 +8,12 @@ from langchain.chat_models import ChatOpenAI
 from langchain.memory import ConversationSummaryBufferMemory
 
 
-#boilerplate code
-from langchain.llms.huggingface_pipeline import HuggingFacePipeline
 
-hf = HuggingFacePipeline.from_model_id(
-    model_id="gpt2",
-    task="text-generation",
-    device = 0,
-    pipeline_kwargs={"max_new_tokens": 1000},
-)
-DEFAULT_LLM = hf
-#end of boilerplate
-pipe = HuggingFacePipeline(pipeline =hf_pipe)
 
 class GptDebateAgent(DebateAgent):
 
     def __init__(self, prompt: str = None):
-        llm = DEFAULT_LLM
+        llm = hugging_face_initiator()
         # llm = GPT4OpenAI(token=environ.get("OPENAPI_SESSION_TOKEN"))
         memory = ConversationSummaryBufferMemory(memory_key="chat_history",return_messages=True, llm=llm)
         super().__init__(llm=llm, memory=memory, prompt=prompt)
